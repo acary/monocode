4285a7cce488e1d380faac8691269d556a064c29
Andy Cary <info@andycary.com>
2024-08-21 09:38:11 -0400

    add router

--- my-turborepo/apps/deno/frontend/monoui/routes/index.tsx
+++ my-turborepo/apps/deno/frontend/monoui/routes/index.tsx
@@ -4,17 +4,17 @@ import Counter from "../islands/Counter.tsx";
 export default function Home() {
   const count = useSignal(3);
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+    <div className="px-4 py-8 mx-auto bg-[#86efac]">
+      <div className="max-w-screen-md mx-auto flex flex-col items-center justify-center">
         <img
-          class="my-6"
+          className="my-6"
           src="/logo.svg"
           width="128"
           height="128"
           alt="Freshly"
         />
-        <h1 class="text-4xl font-bold">Freshly</h1>
-        <p class="my-4">Your interface, as you wish!</p>
+        <h1 className="text-4xl font-bold">Freshly</h1>
+        <p className="my-4">Your interface, as you wish!</p>
         <Counter count={count} />
       </div>
     </div>
